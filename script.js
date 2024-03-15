d2647879e9007541793c5e2728e94736b5882d96
Abdulmajeedalmrebh <mj500500@gmail.com>
2024-03-15 16:56:35 +0300

    Co-authored-by: faisal bin awn <FaisalBinAwn@users.noreply.github.com> Co-authored-by: Kroom00 <Kroom00@users.noreply.github.com>

--- script.js
+++ script.js
@@ -9,17 +9,4 @@ signUpButton.addEventListener("click", () => {
 signInButton.addEventListener("click", () => {
     container.classList.remove("right-panel-active");
 });
-// Function to handle user role selection and redirection
-function chooseRole() {
-    var userType = document.getElementById('userType').value;
 
-    // Hide the landing page
-    document.getElementById('landingPage').style.display = 'none';
-
-    // Redirect to the appropriate page based on the selected user type
-    if (userType === 'student') {
-        window.location.href = 'student.html'; // Replace with the actual filename of your student page
-    } else if (userType === 'teacher') {
-        window.location.href = 'teacher.html'; // Replace with the actual filename of your teacher page
-    }
-}
