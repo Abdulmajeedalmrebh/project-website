fa419c96a0ba9b7c039962d99d5c78641221dae5
Abdulmajeedalmrebh <mj500500@gmail.com>
2024-03-31 14:38:29 +0300

    Co-authored-by: msharialmouslfeh <msharialmouslfeh@users.noreply.github.com> Co-authored-by: Kroom00 <Kroom00@users.noreply.github.com>

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+const bcrypt = require('bcrypt');
+
+const app = express();
+
+app.set('viwe engine' , 'ejs');
+
+
+
+
+
+
+
+
+
+const port = 7000;
+app.listen(port , () =>)
